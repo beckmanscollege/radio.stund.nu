3643904ea66dfe56376c8074fe149c3721be259d
FrankTSzyber <124874739+FrankTSzyber@users.noreply.github.com>
2023-02-22 22:31:34 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,76 +5,60 @@ const message = document.querySelector("#message");
 const hamburgerButton = document.querySelector("#hamburger-button");
 const nav = document.querySelector('nav');
 const infoBtn = document.getElementById('info-btn');
-const infoContent = document.getElementById('info-content');
-
-
-// Use const instead of let for the urls array since it doesn't change.
-const urls = [
-  {
-    url: "https://http-live.sr.se/p1-mp3-192",
-    name: "P1"
-  },
-  {
-    url: "https://http-live.sr.se/p2musik-aac-320",
-    name: "P2 Musik"
-  },
-  {
-    url: "https://http-live.sr.se/p3-mp3-192",
-    name: "P3"
-  },
-  {
-    url: "https://http-live.sr.se/p4stockholm-mp3-192",
-    name: "P4 Stockholm"
-  },
-  {
-    url: "https://ice2.somafm.com/missioncontrol-128-mp3",
-    name: "Mission Control"
-  },
-  {
-    url: "https://audiotainment-sw.streamabc.net/atsw-oldschoolhiphop-mp3-128-3940912?sABC=63ronq5r%230%23n2382qro0prr58q1pqqr25378sors240%23enqvbqr&aw_0_1st.playerid=radiode&amsparams=playerid:radiode;skey:1676389726",
-    name: "bigFM Oldschool Rap & Hip-Hop"
-  },
-  {
-    url: "https://strw1.openstream.co/938?",
-    name: "House & Techno Classics"
-  },
-  {
-    url: "https://listen.soulradio.us/us",
-    name: "Soul Radio Classics US"
-  },
-  {
-    url: "http://fm05-ice.stream.khz.se/fm05_mp3",
-    name: "Star FM"
-  },
-  {
-    url: "http://wr08-ice.stream.khz.se/wr08_mp3",
-    name: "Soul Classics SWE"
-  }
-];
+const infoContent = document.getElementById('info-content-allt');
+const songList = document.querySelectorAll('ul li a');
 
 let currentIndex = 0;
+let currentPlayingIndex = currentIndex;
+let isFirstNextClick = false;
+
+
+
+const urls = Array.from(songList).map(song => {
+  return {url: song.getAttribute('data-sound'), name: song.textContent};
+});
 
 player.src = urls[currentIndex].url;
 
-prevTriangle.addEventListener("click", function() {
-  currentIndex--;
-  if (currentIndex < 0) {
-    currentIndex = urls.length - 1;
-  }
-  player.src = urls[currentIndex].url;
-  player.play();
-  showMessage("Playing: " + urls[currentIndex].name);
-});
-
-nextTriangle.addEventListener("click", function() {
-  currentIndex++;
-  if (currentIndex === urls.length) {
-    currentIndex = 0;
-  }
+function playSong(index) {
+  currentIndex = index;
   player.src = urls[currentIndex].url;
   player.play();
   showMessage(urls[currentIndex].name);
+}
 
+prevTriangle.addEventListener("click", function() {
+  let prevIndex = currentIndex - 1;
+  if (prevIndex < 0) {
+    prevIndex = urls.length - 1;
+  }
+  playSong(prevIndex);
+});
+
+nextTriangle.addEventListener("click", function() {
+if (!isFirstNextClick) {
+playSong(0);
+isFirstNextClick = true;
+} else {
+let nextIndex = currentIndex + 1;
+if (nextIndex === urls.length) {
+playSong(0);
+} else {
+playSong(nextIndex);
+}
+}
+});
+
+nav.addEventListener('click', (event) => {
+  if (event.target.tagName === 'A') {
+    event.preventDefault();
+    const index = Array.from(songList).findIndex(song => song === event.target);
+    playSong(index);
+    nav.style.opacity = 0;
+    setTimeout(() => {
+      nav.classList.remove('show');
+    }, 1000);
+  }
 });
 
 function showMessage(text) {
@@ -84,6 +68,8 @@ function showMessage(text) {
     message.style.opacity = 0;
   }, 2500);
 }
+
+
 
 hamburgerButton.addEventListener('click', () => {
   nav.classList.toggle('show');
@@ -97,24 +83,18 @@ hamburgerButton.addEventListener('click', () => {
   
 });
 
-nav.addEventListener('click', (event) => {
-  if (event.target.tagName === 'A') {
-    event.preventDefault();
-    const soundUrl = event.target.getAttribute('data-sound');
-    player.src = soundUrl;
-    player.play();
-     nav.style.opacity = 0;
-    setTimeout(() => {
-      nav.classList.remove('show');
-    }, 1000);
-     showMessage(event.target.textContent);
-  }
-});
-
-
 
 document.addEventListener('click', (event) => {
   if (event.target !== hamburgerButton && !nav.contains(event.target)) {
+    nav.style.opacity = 0;
+    setTimeout(() => {
+      nav.classList.remove('show');
+    }, 1000); 
+  }
+});
+
+nav.addEventListener('click', (event) => {
+  if (nav.classList.contains('show')) {
     nav.style.opacity = 0;
     setTimeout(() => {
       nav.classList.remove('show');
@@ -130,7 +110,7 @@ infoBtn.addEventListener('click', function() {
   let intervalID = setInterval(function() {
     opacity += 0.05;
     infoContent.style.opacity = opacity;
-    if (opacity >= 0.5) {
+    if (opacity >= 0.7) {
       clearInterval(intervalID);
     }
   }, 50);
@@ -150,3 +130,30 @@ infoContent.addEventListener('click', function(event) {
   }
 });
 
+const infoContent2 = document.querySelector('.info-content2');
+
+infoContent2.addEventListener('click', function() {
+  let opacity = 0.5;
+  let intervalID = setInterval(function() {
+    opacity -= 0.05;
+    infoContent.style.opacity = opacity;
+    if (opacity <= 0) {
+      clearInterval(intervalID);
+      infoContent.style.display = 'none';
+    }
+  }, 50);
+});
+
+
+
+
+const volumeSlider = document.querySelector("#volume");
+
+volumeSlider.addEventListener('input', (event) => {
+  const value = event.target.value;
+  const percentage = (value - event.target.min) / (event.target.max - event.target.min) * 100;
+  volumeSlider.style.setProperty('--value', `${percentage}%`);
+});
+volumeSlider.addEventListener("input", () => {
+  player.volume = volumeSlider.value;
+});
